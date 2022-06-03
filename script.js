6d35de8267b07740d6ad52e9388920dedfb42ffa
Sela Privette <selaprivette@yahoo.com>
2022-06-02 21:00:32 -0400

    js done

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var formSubmitHandler = function (event) {
     if (city) {
         getCityWeather(city);
         get5Day(city);
-        cities.push({city});
+        cities.push({ city });
         cityEstablishedEl.value = '';
     } else {
         alert('PLEASE ENTER CITY');
@@ -31,13 +31,13 @@ var saveSearch = function () {
 var getCityWeather = function (city) {
     var apiKey = '8b62e1bd62c44925cbf6e9bb8f211dba'
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}'
-    
+
     fetch(apiUrl)
-    .then(function (response) {
-        response.json().then(function (data) {
-            displayWeather(data, city);
+        .then(function (response) {
+            response.json().then(function (data) {
+                displayWeather(data, city);
+            });
         });
-    });
 
 };
 //clear search//
@@ -52,7 +52,7 @@ var displayWeather = function (weather, searchCity) {
 
     //weather symbols//
     var weatherSymbol = document.createElement('img')
-    weatherSymbol.setAttribute('src', `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` );
+    weatherSymbol.setAttribute('src', `https://openweathermap.org/img/wn/${weather.weather[0].Symbol}@2x.png`);
     citySearchedEl.appendChild(weatherSymbol);
 
     //temp section//
@@ -64,12 +64,12 @@ var displayWeather = function (weather, searchCity) {
     var humidityEl = document.createElement("section");
     humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
     humidityEl.classList = "list-group-item"
-    
+
     //wind section//
     var windEl = document.createElement("section");
     windEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
     windEl.classList = "list-group-item"
-    
+
     //append all to weather box//
     weatherBoxEl.appendChild(tempEl);
     weatherBoxEl.appendChild(humidityEl);
@@ -97,8 +97,88 @@ var displayUvIndex = function (index) {
     var uvIndexEl = document.createElement('section');
     uvIndexEl.textContent = 'UV Index:'
     uvIndexEl.classList = 'list-group-item'
-    
+
     uvIndexValue = document.createElement('section')
     uvIndexValue.textContent = index.value
-    
+
+    if (index.value <= 2) {
+        uvIndexValue.classList = 'favorable'
+    } else if (index.value > 2 && index.value <= 8) {
+        uvIndexValue.classList = 'moderate'
+    }
+    else if (index.value > 8) {
+        uvIndexValue.classList = 'severe'
+    };
+
+    uvIndexEl.appendChild(uvIndexValue);
+    weatherBoxEl.appendChild(uvIndexEl);
 }
+var get5Day = function (city) {
+    var apiKey = "8b62e1bd62c44925cbf6e9bb8f211dba"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+        .then(function (response) {
+            response.json().then(function (data) {
+                display5Day(data);
+            });
+        });
+};
+var display5Day = function (weather) {
+    forecastBoxEl.textContent = ""
+    forecastHeader.textContent = "5-Day Forecast:";
+
+    var forecast = weather.list;
+    for (var i = 5; i < forecast.length; i = i + 8) {
+        var dailyForecast = forecast[i];
+
+        var forecastEl = document.createElement("section");
+        forecastEl.classList = "card bg-primary text-light m-2";
+
+        //date section//
+        var forecastDate = document.createElement("h5")
+        forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center"
+        forecastEl.appendChild(forecastDate);
+
+        //weather symbols//
+        var weatherSymbol = document.createElement("img")
+        weatherSymbol.classList = "card-body text-center";
+        weatherSymbol.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].Symbol}@2x.png`);
+        forecastEl.appendChild(weatherSymbol);
+
+        //forcast temp section//
+        var forecastTempEl = document.createElement("section");
+        forecastTempEl.classList = "card-body text-center";
+        forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+        forecastEl.appendChild(forecastTempEl);
+        //forecast humidity section//
+        var forecastHumidityEl = document.createElement("section");
+        forecastHumidityEl.classList = "card-body text-center";
+        forecastHumidityEl.textContent = dailyForecast.main.humidity + "  %";
+        forecastEl.appendChild(forecastHumidityEl);
+        forecastContainerEl.appendChild(forecastEl);
+    }
+
+}
+//previous search buttons//
+var previousSearch = function (previousSearch) {
+    previousSearchEl = document.createElement("button");
+    previousSearchEl.textContent = previousSearch;
+    previousSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    previousSearchEl.setAttribute("data-city", previousSearch)
+    previousSearchEl.setAttribute("type", "submit");
+
+    previousSearchButtonEl.prepend(previousSearchEl);
+}
+var previousSearchHandler = function (event) {
+    var city = event.target.getAttribute("data-city")
+    if (city) {
+        getCityWeather(city);
+        get5Day(city);
+    }
+}
+//event listeners//
+cityFormEl.addEventListener("submit", formSumbitHandler);
+previousSearchButtonEl.addEventListener("click", previousSearchHandler);
+
